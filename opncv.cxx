2ecf487e790248992c1afe24a28c59331fdf35b1
mazispider <mazispider@gmail.com>
2020-03-12 00:37:49 +0330

    Add files via upload

---
+++ opncv.cxx
@@ -0,0 +1,32 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, char** argv )
+{	
+	
+    if( argc == 1)
+    {
+     cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+     return -1;
+    }
+
+    Mat image;
+    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
+
+    if(! image.data )                              // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+	cv::Vec3b pixel = image.at<cv::Vec3b>(atoi(argv[2]), atoi(argv[3]));
+	printf("%d,%d,%d\n",pixel[0],pixel[1],pixel[1]);
+    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", image );                   // Show our image inside it.
+
+    waitKey(0);                                          // Wait for a keystroke in the window
+    return 0;
+}
